5abac5c9127c91239c9499f92c882b4974d66514
TanmayiKasthuri <67643608+TanmayiKasthuri@users.noreply.github.com>
2024-07-16 16:28:35 -0400

    Update settings.py

--- cookbook/settings.py
+++ cookbook/settings.py
@@ -30,8 +30,7 @@ ALLOWED_HOSTS = ['*']
 CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
-    "http://127.0.0.1:3000",
-    "https://54.234.66.83:8000/"
+    "http://127.0.0.1:3000"
 ]
 
 REST_FRAMEWORK = {
